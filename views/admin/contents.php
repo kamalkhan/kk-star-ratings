8e19389b74304359f469e3aeaedfae910f172b02
Kamal Khan <shout@bhittani.com>
2019-07-05 07:11:48 +0500

    Make sure correct settings tab is updated

--- views/admin/contents.php
+++ views/admin/contents.php
@@ -1,4 +1,4 @@
-<form action="options.php" method="POST" style="margin: 2rem;">
+<form action="options.php?tab=<?php echo $active; ?>" method="POST" style="margin: 2rem;">
     <?php settings_fields(KKSR_SLUG); ?>
     <?php do_settings_sections(KKSR_SLUG); ?>
     <?php submit_button(); ?>
