7fb953fccfce2a1f88e86d827ec6b5c54c06a0de
Kamal Khan <shout@bhittani.com>
2019-07-06 23:24:29 +0500

    Use width and height in markup. Add alignment styles.

--- views/star.php
+++ views/star.php
@@ -1,3 +1,4 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" stroke-width="1">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"
+    stroke-width="1" width="<?php echo $size; ?>" height="<?php echo $size; ?>">
     <path d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z" />
 </svg>
