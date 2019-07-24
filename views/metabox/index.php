20ee76de6d046541ed381e8e4bb02aa382405e89
Kamal Khan <shout@bhittani.com>
2019-07-24 10:17:38 +0500

    Touch metabox

--- views/metabox/index.php
+++ views/metabox/index.php
@@ -13,21 +13,23 @@
                 <?php _e('Reset Ratings', 'kk-star-ratings'); ?>
             </label>
         </div>
+    </div>
 
-        <label class="components-base-control__label" style="margin-top: 1rem;">
-            <?php _e('Status', 'kk-star-ratings'); ?>
+    <div class='components-base-control__field'>
+        <label class="components-base-control__label" style="margin-top: .75rem; margin-bottom: .25rem;">
+            <strong><?php _e('Status', 'kk-star-ratings'); ?></strong>
         </label>
 
         <div style="margin-top: 1rem;">
-            <label class="components-base-control__label" style="margin-top: .75rem; margin-bottom: .25rem;">
+            <label class="components-base-control__label" style="margin-top: .5rem; margin-bottom: .25rem;">
                 <input type="radio" name="<?php echo $statusFieldName; ?>" value="" <?php checked($status, ''); ?>>
                 <?php _e('Auto', 'kk-star-ratings'); ?>
             </label>
-            <label class="components-base-control__label" style="margin-top: .75rem; margin-bottom: .25rem;">
+            <label class="components-base-control__label" style="margin-top: .5rem; margin-bottom: .25rem;">
                 <input type="radio" name="<?php echo $statusFieldName; ?>" value="enable" <?php checked($status, 'enable'); ?>>
                 <?php _e('Enable', 'kk-star-ratings'); ?>
             </label>
-            <label class="components-base-control__label" style="margin-top: .75rem; margin-bottom: .25rem;">
+            <label class="components-base-control__label" style="margin-top: .5rem; margin-bottom: .25rem;">
                 <input type="radio" name="<?php echo $statusFieldName; ?>" value="disable" <?php checked($status, 'disable'); ?>>
                 <?php _e('Disable', 'kk-star-ratings'); ?>
             </label>
