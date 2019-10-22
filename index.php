b46ab5fee4d7676efb07ab1bf3af6c3fcf53a7fb
Kamal Khan <shout@bhittani.com>
2019-10-22 07:39:13 +0500

    Deprecate use of custom get and set options

--- index.php
+++ index.php
@@ -17,22 +17,4 @@ if (! defined('ABSPATH')) {
     die();
 }
 
-define('KKSR_FILE', __FILE__);
-define('KKSR_PLUGIN', plugin_basename(KKSR_FILE));
-define('KKSR_VERSION', '4.0.0-alpha');
-define('KKSR_PREFIX', 'kksr_');
-define('KKSR_SLUG', 'kk-star-ratings');
-define('KKSR_LABEL', 'kk Star Ratings');
-define('KKSR_SHORTCODE', 'kkstarratings');
-define('KKSR_NAMESPACE', 'Bhittani\StarRating\\');
-define('KKSR_PATH', plugin_dir_path(KKSR_FILE));
-define('KKSR_PATH_SRC', KKSR_PATH . 'src/');
-define('KKSR_PATH_VIEWS', KKSR_PATH . 'views/');
-define('KKSR_PATH_PUBLIC', KKSR_PATH . 'public/');
-define('KKSR_ASSET', plugin_dir_url(KKSR_FILE) . 'public/');
-
-foreach ([
-    'functions.php',
-] as $src) {
-    require_once KKSR_PATH_SRC.$src;
-}
+//
