15c97a148d0d9aa560d7e48d7f4bc7471c95e782
Kamal Khan <shout@bhittani.com>
2019-10-26 12:14:49 +0500

    Respect rtl layout. Allow vote count i18n.

--- views/legend.php
+++ views/legend.php
@@ -5,7 +5,9 @@
         <strong><?= $best ?></strong>
         <span class="kksr-muted">(</span>
         <strong class="kksr-count"><?= $count ?></strong>
-        <span class="kksr-muted">votes</span>
+        <span class="kksr-muted">
+            <?= _n('vote', 'votes', $count, 'kk-star-ratings') ?>
+        </span>
         <span class="kksr-muted">)</span>
     <?php else : ?>
         <span class="kksr-muted"><?= $greet ?></span>
